a231d0e98a0230284b1f9f00070fe42c5d2eaf64
Yasin hessnawi <yasinhessnawi1@gmail.com>
2024-07-27 16:29:13 +0200

    i forgot to delete the id handler file

--- models/torrent.go
+++ models/torrent.go
@@ -1,7 +1,5 @@
 package models
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
-
 type TorrentResponse struct {
 	Data  []Torrent `json:"data"`
 	Time  float64   `json:"time"`
@@ -9,16 +7,15 @@ type TorrentResponse struct {
 }
 
 type Torrent struct {
-	ID       primitive.ObjectID `bson:"_id,omitempty"  json:"id,omitempty"`
-	Name     string             `bson:"name" json:"name"`
-	Size     string             `bson:"size" json:"size"`
-	Date     string             `bson:"date" json:"date"`
-	Seeders  string             `bson:"seeders" json:"seeders"`
-	Leechers string             `bson:"leechers" json:"leechers"`
-	Url      string             `bson:"url" json:"url"`
-	Uploader string             `bson:"uploader" json:"uploader"`
-	Category string             `bson:"category" json:"category"`
-	Poster   string             `bson:"poster" json:"poster"`
-	Magnet   string             `bson:"magnet" json:"magnet"`
-	Hash     string             `bson:"hash" json:"hash"`
+	Name     string `bson:"name" json:"name"`
+	Size     string `bson:"size" json:"size"`
+	Date     string `bson:"date" json:"date"`
+	Seeders  string `bson:"seeders" json:"seeders"`
+	Leechers string `bson:"leechers" json:"leechers"`
+	Url      string `bson:"url" json:"url"`
+	Uploader string `bson:"uploader" json:"uploader"`
+	Category string `bson:"category" json:"category"`
+	Poster   string `bson:"poster" json:"poster"`
+	Magnet   string `bson:"magnet" json:"magnet"`
+	Hash     string `bson:"hash" json:"hash"`
 }
