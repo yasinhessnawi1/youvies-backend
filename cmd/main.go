9532ebff87edc75dc53fa1f23881599c94e6b408
Yasin hessnawi <yasinhessnawi1@gmail.com>
2024-08-27 22:26:28 +0200

    enabled cors

--- cmd/main.go
+++ cmd/main.go
@@ -38,7 +38,7 @@ func main() {
 
 func enableCors(c *gin.Context) {
 	c.Writer.Header().Set("Access-Control-Allow-Origin", "https://youvies.online")
-	c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+	//c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
 	c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 	c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
